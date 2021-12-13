165ed47ed76b3cfcb9477c25a798ce1e874b1835
bujjip <bujjip566@gmail.com>
2021-12-13 14:48:43 +0530

    Add project files.

---
+++ work2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("learning github");
+        }
+    }
+}
